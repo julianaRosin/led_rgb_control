ea54c307403500fc3481c6c0c1e39378d99f2118
Juliana Rosin <juliana.frosin@gmail.com>
2019-09-30 22:07:55 -0300

    codigo arduino

---
+++ led_control.ino
@@ -0,0 +1,39 @@
+String cor;
+String red;
+String green;
+String blue;
+int R,G,B;
+
+//Function color
+
+void color_led(int r, int g, int b){
+  analogWrite(9,255-b);
+  analogWrite(10,255-g);
+  analogWrite(11,255-r);
+}
+ 
+void setup()
+{
+   Serial.begin(9600);
+  
+   pinMode(9, OUTPUT); //blue
+   pinMode(10, OUTPUT);//green
+   pinMode(11, OUTPUT); //red
+}
+ 
+void loop()
+{
+  if(Serial.available()){
+    cor = Serial.readString();
+    red = cor.substring(0,3);
+    green = cor.substring(3,6);
+    blue = cor.substring(6,9); 
+    R = red.toInt();
+    G = green.toInt();
+    B = blue.toInt();
+    Serial.print(R);
+    Serial.print(G);
+    Serial.print(B);
+    color_led(R,G,B);
+  } 
+}
